4484a507e57b206fa5e514077edd4a5dd06d2677
landen <landenjwilliams@gmail.com>
2023-01-23 00:29:12 -0600

    game working

--- app.js
+++ app.js
@@ -1,57 +1,109 @@
+//dom selectors & various variables
+
 const board = document.getElementById('board');
-board.addEventListener('click', onBoardClick); // etc
-
-// state
+const playerText = document.getElementById ('player');
+const restartButton = document.getElementById('restartbutton');
+const winConditions = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 const gameState = {
-    players: ['x', 'o'],
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ],
-    winningConditions: [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ]
+    players: ['X', 'O'],
+    board: ["", "", "", "", "", "", "", "", ""],
   }
+let players = gameState.players.map(i => i);
+let playerOne = players[0];
+let playerTwo = players[1];
+let currentPlayer = playerOne;
+let spaces = gameState.board.map(i => i);
 
+
+//helper functions
+
+const switchPlayer = () => {
+  
+  if(currentPlayer == playerOne){
+    currentPlayer = playerTwo;
+  }
+  else {
+    currentPlayer = playerOne;
+  };
+  playerText.innerText = `Player: ${currentPlayer}`;
+}
+
+const createSpace = (i) => {
  
-function buildInitialState() {
+  const box = document.createElement('div');
+  box.className = "box";
+  box.id = i++;
+  board.appendChild(box);
+  box.addEventListener('click', (event) => {
+      clickFunction(currentPlayer, box);
+    })
+ 
+  restartButton.addEventListener('click', () => {
+    restartGame(box);
+  });
+};
 
+const clickFunction = (currentPlayer, box) => {
+  box.innerText = `${currentPlayer}`;
+  spaces[box.id] = currentPlayer;
+  checkWinner();
 }
 
-// render
-function renderState() {
 
-}
+const createGameboard = () => {
+  for (let i = 0; i <= 8; i++) {
+    createSpace(i);
+  };
+};
 
-// maybe a dozen or so helper functions for tiny pieces of the interface
+const checkWinner =() => {
+  let roundWon = false;
 
-// listeners
-function onBoardClick() {
-  // update state, maybe with another dozen or so helper functions...
+  for(let i = 0; i < winConditions.length; i++){
 
-  renderState() // show the user the new state
-}
+      const condition = winConditions[i];
+      const spaceA = spaces[condition[0]];
+      const spaceB = spaces[condition[1]];
+      const spaceC = spaces[condition[2]];
 
-
-
-// add to above
-function tick() {
-    // this is an incremental change that happens to the state every time you update...
-  
-    renderState()
+      if(spaceA == "" || spaceB == "" || spaceC == ""){
+          continue;
+      }
+      if(spaceA == spaceB && spaceB == spaceC){
+          roundWon = true;
+          break;
+      }
   }
-  
-  setInterval(tick, 1000 / 30) // as close to 30 frames per second as possible
-  
-  // now you might have things like
-  document.addEventListener('keydown', function (event) {
-    // here you might read which key was pressed and update the state accordingly
-  })
+
+  if(roundWon){
+    playerText.innerText = `${currentPlayer} wins!`;
+  } else if(!spaces.includes("")){
+    playerText.innerText = `Draw!`;
+  } else{
+    switchPlayer();
+  }
+}
+
+const restartGame = (box) => {
+  box.innerText = "";
+  currentPlayer = playerOne;
+  spaces = ["", "", "", "", "", "", "", "", ""];
+  playerText.innerText = `Player: ${currentPlayer}`; 
+
+};
+
+
+// render game
+
+createGameboard();
+
+
